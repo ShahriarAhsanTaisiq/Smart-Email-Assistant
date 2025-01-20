5c2effe2aa989105129f60e58953edd9b31239db
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2025-01-21 02:36:17 +0600

    Implements EmailGeneratorController.

--- emailAssistance/src/main/java/com/email/Assistance/controller/EmailGeneratorController.java
+++ emailAssistance/src/main/java/com/email/Assistance/controller/EmailGeneratorController.java
@@ -1,10 +1,29 @@
 package com.email.Assistance.controller;
 
+import com.email.Assistance.model.EmailRequest;
+import com.email.Assistance.service.EmailGeneratorService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/email")
 public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest ) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
 
 }
