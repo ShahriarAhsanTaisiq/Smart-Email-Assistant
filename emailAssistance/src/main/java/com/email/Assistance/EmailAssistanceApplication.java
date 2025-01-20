46af40d344bfbe8ad5f23fbf248875bda9cfb1ac
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2025-01-21 02:38:08 +0600

    Add all the additional files

---
+++ emailAssistance/src/main/java/com/email/Assistance/EmailAssistanceApplication.java
@@ -0,0 +1,11 @@
+package com.email.Assistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAssistanceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAssistanceApplication.class, args);
+	}
+}
