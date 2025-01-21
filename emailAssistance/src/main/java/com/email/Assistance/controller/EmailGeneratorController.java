05b91a693f52397d9b25abc0c5da69cb1a4a40d3
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2025-01-22 00:12:34 +0600

    Enable CROS origins

--- emailAssistance/src/main/java/com/email/Assistance/controller/EmailGeneratorController.java
+++ emailAssistance/src/main/java/com/email/Assistance/controller/EmailGeneratorController.java
@@ -5,13 +5,11 @@ import com.email.Assistance.service.EmailGeneratorService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
